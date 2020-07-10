913504e2580486ca9308db1e3f1ee86cf85301df
lucas.volgarini <lucas.volgarini@senior.com.br>
2020-07-10 09:04:05 -0300

    Fechamento da versão: 2.3.1.RELEASE

--- src/main/java/br/com/xyinc/br/com/xyinc/domain/Version.java
+++ src/main/java/br/com/xyinc/br/com/xyinc/domain/Version.java
@@ -9,7 +9,7 @@ public class Version {
 	 * 'changeversion.sh'
 	 */
 	public static final String VERSAO = "1.0.0";
-	public static final String BUILDTIME = "08/07/2020 22:26";
+	public static final String BUILDTIME = "10/07/2020 09:04";
 
 	public static final String NOME_SISTEMA = "XY Inc";
 	public static final String DESCRICAO_SISTEMA = "Serviço Gerenciamento GPS";
