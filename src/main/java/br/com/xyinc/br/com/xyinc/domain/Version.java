9308a7fe0cf54ae7d26ace5522bd172bd4b353c6
lucas.volgarini <lucas.volgarini@senior.com.br>
2020-07-10 09:16:56 -0300

    Alterando para versao 1.0.2-SNAPSHOT

--- src/main/java/br/com/xyinc/br/com/xyinc/domain/Version.java
+++ src/main/java/br/com/xyinc/br/com/xyinc/domain/Version.java
@@ -8,8 +8,8 @@ public class Version {
 	 * ESSE PARAMETRO NÃO DEVE SER MODIFICADO MANUALMENTE, UTILIZE O SCRIPT
 	 * 'changeversion.sh'
 	 */
-	public static final String VERSAO = "1.0.1";
-	public static final String BUILDTIME = "10/07/2020 09:15";
+	public static final String VERSAO = "1.0.2";
+	public static final String BUILDTIME = "10/07/2020 09:16";
 
 	public static final String NOME_SISTEMA = "XY Inc";
 	public static final String DESCRICAO_SISTEMA = "Serviço Gerenciamento GPS";
