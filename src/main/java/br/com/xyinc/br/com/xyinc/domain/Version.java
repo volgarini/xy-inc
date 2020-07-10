1eb0fdaa88c1229c582216224492a2318494e19d
lucas.volgarini <lucas.volgarini@senior.com.br>
2020-07-10 09:14:47 -0300

    Fechamento da versão: 1.0.0-SNAPSHOT.1-SNAPSHOT

--- src/main/java/br/com/xyinc/br/com/xyinc/domain/Version.java
+++ src/main/java/br/com/xyinc/br/com/xyinc/domain/Version.java
@@ -9,7 +9,7 @@ public class Version {
 	 * 'changeversion.sh'
 	 */
 	public static final String VERSAO = "1.0.0.1-SNAPSHOT";
-	public static final String BUILDTIME = "10/07/2020 09:12";
+	public static final String BUILDTIME = "10/07/2020 09:14";
 
 	public static final String NOME_SISTEMA = "XY Inc";
 	public static final String DESCRICAO_SISTEMA = "Serviço Gerenciamento GPS";
