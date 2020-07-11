83b626661d8824485c381b3686d4142ab704dda5
Lucas Volgarini <lucas.volgarini@gmail.com>
2020-07-11 10:33:08 -0300

    -alteração para executar a api via docker

--- src/test/java/br/com/xyinc/XyIncApplicationTests.java
+++ src/test/java/br/com/xyinc/XyIncApplicationTests.java
@@ -3,9 +3,7 @@ package br.com.xyinc;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class XyIncApplicationTests {
 
 	@Test
